3d1927b0c02f782e48a8ae610a08ce2da1959cc2
이건우 <gunwoo.lee1@cj.net>
2022-11-23 11:40:19 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/cj/devops/domin/domainexpiration/DomainExpirationApplication.java
@@ -0,0 +1,13 @@
+package com.cj.devops.domin.domainexpiration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainExpirationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainExpirationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cj/devops/domin/domainexpiration/DomainExpirationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cj.devops.domin.domainexpiration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainExpirationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
